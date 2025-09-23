071b34e9ef4a36f51a84cf25d887fc335658a65b
Caio Andres <caioandresgit@outlook.com>
2025-09-23 20:31:40 -0300

    feat(agent/system prompt): PDI agent and his own system prompt created.

---
+++ app/src/agents/pdi_agent.py
@@ -0,0 +1,19 @@
+from app.src.agents.system_prompt.system_prompt_pdi_agent import SYSTEM_PROMPT_PDI_AGENT
+from app.src.util.current_datetime import current_datetime
+from app.src.auth.openai import client
+
+
+user_prompt = input("Diga sobre você: ")
+
+
+def pdi_agent():
+    message = client.responses.create(
+        model="gpt-4",
+        input=[
+            {"role": "developer", "content": f"Data e tempo atual: {current_datetime}"},
+            {"role": "developer", "content": SYSTEM_PROMPT_PDI_AGENT},
+            {"role": "user", "content": user_prompt},
+        ],
+    )
+
+    return message.output_text
